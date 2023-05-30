6d9b9bbcc89dd4b8724ac631d3ec1f63180a3114
Pablo Vieyra <Pablin_vieyra1@outlook.com>
2023-05-29 22:53:12 -0300

    Config initial project

---
+++ src/main/java/com/grupo14/oob2/Oob2Application.java
@@ -0,0 +1,13 @@
+package com.grupo14.oob2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oob2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oob2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/grupo14/oob2/Oob2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo14.oob2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oob2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
