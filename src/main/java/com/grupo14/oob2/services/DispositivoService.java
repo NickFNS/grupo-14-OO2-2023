830a654827cdc339691adffeaa9f7f9f67266848
NickFNS <fsierra7180@gmail.com>
2023-06-20 20:10:10 -0300

    Minor code changes

--- src/main/java/com/grupo14/oob2/services/DispositivoService.java
+++ src/main/java/com/grupo14/oob2/services/DispositivoService.java
@@ -5,7 +5,6 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.web.bind.annotation.ModelAttribute;
 
 import com.grupo14.oob2.entities.Banio;
 import com.grupo14.oob2.entities.Dispositivo;
